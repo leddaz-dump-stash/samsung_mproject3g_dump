c6d26441ed29f8977def3f6d3467bf97e3e19b5a
Leonardo Ledda <leonardoledda@gmail.com>
2022-09-16 21:10:04 +0000

    Add mproject3gxx-user 4.4.2 KOT49H C101XXUBNH1 release-keys
    Signed-off-by: Leonardo Ledda <leonardoledda@gmail.com>

---
+++ README.md
@@ -0,0 +1,14 @@
+# mproject3gxx-user 4.4.2 KOT49H C101XXUBNH1 release-keys
+- manufacturer: samsung
+- platform: exynos4
+- codename: mproject3g
+- flavor: user
+- release: 4.4.2
+- id: KOT49H
+- incremental: C101XXUBNH1
+- tags: release-keys
+- fingerprint: samsung/mproject3gxx/mproject3g:4.4.2/KOT49H/C101XXUBNH1:user/release-keys
+- is_ab: false
+- brand: samsung
+- branch: mproject3gxx-user-4.4.2-KOT49H-C101XXUBNH1-release-keys
+- repo: samsung_mproject3g_dump
---
+++ twrp-device-tree/samsung/mproject3g/omni_mproject3g.mk
@@ -0,0 +1,25 @@
+#
+# Copyright (C) 2022 The Android Open Source Project
+# Copyright (C) 2022 SebaUbuntu's TWRP device tree generator
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+# Inherit from those products. Most specific first.
+$(call inherit-product-if-exists, $(SRC_TARGET_DIR)/product/embedded.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/languages_full.mk)
+
+# Inherit from mproject3g device
+$(call inherit-product, device/samsung/mproject3g/device.mk)
+
+# Inherit some common Omni stuff.
+$(call inherit-product, vendor/omni/config/common.mk)
+$(call inherit-product, vendor/omni/config/gsm.mk)
+
+# Device identifier. This must come after all inclusions
+PRODUCT_DEVICE := mproject3g
+PRODUCT_NAME := omni_mproject3g
+PRODUCT_BRAND := samsung
+PRODUCT_MODEL := SM-C101
+PRODUCT_MANUFACTURER := samsung
